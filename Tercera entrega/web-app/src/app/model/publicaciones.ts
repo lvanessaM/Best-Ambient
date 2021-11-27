542e43e0cf670e03c7769ce8b30bd9eaeac149c3
Bryanok <bsariza6@misena.edu.co>
2021-11-27 04:01:39 -0500

    Doc.se actualiza archivo 3ra entrega

--- Tercera entrega/web-app/src/app/model/publicaciones.ts
+++
@@ -1,8 +0,0 @@
-import { Usuarios } from 'src/app/model/usuarios';
-export class Publicaciones {
-  Titulo: string;
-  Descripcion: string;
-  estado!: boolean;
-  fecha: Date;
-  Usuario: Usuarios[];
-}
